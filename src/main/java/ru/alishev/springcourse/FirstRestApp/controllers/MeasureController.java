93cb83c853ec935bc5b4893a4de97b0ca1083e74
Igor_Shpilevsky <admin@itproger.com>
2023-04-27 18:26:30 +0300

    I have finished server side

--- src/main/java/ru/alishev/springcourse/FirstRestApp/controllers/MeasureController.java
+++ src/main/java/ru/alishev/springcourse/FirstRestApp/controllers/MeasureController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.alishev.springcourse.FirstRestApp.consumer.Consumer;
 import ru.alishev.springcourse.FirstRestApp.dto.MeasureDTO;
 import ru.alishev.springcourse.FirstRestApp.models.Measure;
 import ru.alishev.springcourse.FirstRestApp.services.MeasureService;
